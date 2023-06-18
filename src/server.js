45be622bde978519225171e6a9cf1b60069f0616
Widiskel <waputra16@gmail.com>
2023-06-18 17:31:10 +0700

    implements jwt token base auth

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const Hapi = require('@hapi/hapi');
+
 const albumPlugin = require('./api/album');
-const songPlugin = require('./api/song');
-const usersPlugin = require('./api/users');
 const AlbumValidator = require('./validator/album');
 const AlbumService = require('./services/repository/AlbumService');
+
+const songPlugin = require('./api/song');
 const SongService = require('./services/repository/SongService');
 const SongValidator = require('./validator/song');
+
 const UsersService = require('./services/repository/UsersService');
-const ClientError = require('./exceptions/ClientError');
+const usersPlugin = require('./api/users');
 const UsersValidator = require('./validator/users');
+
+const auth = require('./api/auth');
+const AuthService = require('./services/repository/AuthService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthValidator = require('./validator/auth');
+
+const ClientError = require('./exceptions/ClientError');
 
 require('dotenv').config();
 
@@ -16,6 +25,8 @@ const init = async () => {
   const albumService = new AlbumService();
   const songService = new SongService();
   const usersService = new UsersService();
+  const authService = new AuthService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -50,6 +61,15 @@ const init = async () => {
           service: usersService,
           validator: UsersValidator,
         },
+    },
+    {
+      plugin: auth,
+      options: {
+        authService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthValidator,
+      },
     },
   ]);
 
